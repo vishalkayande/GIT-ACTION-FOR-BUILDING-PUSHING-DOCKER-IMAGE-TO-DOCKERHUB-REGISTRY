481bce36866b603384b056db4485abe9b6096ab7
Mayur Nikam <146607259+mayurnikam266@users.noreply.github.com>
2026-06-25 20:46:58 +0530

    adding running step

---
+++ docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Variables
+IMAGE_NAME=my-nginx
+CONTAINER_NAME=my-nginx-container
+HOST_PORT=8080
+CONTAINER_PORT=80
+
+echo "Building Docker image..."
+docker build -t $IMAGE_NAME .
+
+echo "Stopping existing container (if any)..."
+docker rm -f $CONTAINER_NAME 2>/dev/null
+
+echo "Running container..."
+docker run -d \
+  --name $CONTAINER_NAME \
+  -p $HOST_PORT:$CONTAINER_PORT \
+  $IMAGE_NAME
+
